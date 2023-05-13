1f3a543ce0205248ae00b8a08876bb1569dd7bfc
Plamena Mihaylova <plamena.pavlova.mihaylova@gmail.com>
2023-05-13 09:40:44 +0300

    Update randomized queue

--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -1,25 +1,25 @@
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private static final int INIT_CAPACITY = 8;
     private Item[] queue;
-    private int tail;
-    private int head;
     private int size;
 
 
+    /**
+     * Construct an empty randomized queue.
+     */
     public RandomizedQueue() {
-        queue = (Item[]) new Object[1];
-        head = 0;
-        tail = 0;
+        queue = (Item[]) new Object[INIT_CAPACITY];
         size = 0;
     }
 
     /**
+     * Check if the randomized queue is empty.
+     *
      * @return {@code true} if the current randomized queue is empty; {@code false} otherwise
      */
     public boolean isEmpty() {
@@ -27,6 +27,8 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     /**
+     * Return the size of the randomized queue.
+     *
      * @return the number of items in the current randomized queue
      */
     public int size() {
@@ -34,23 +36,28 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     /**
-     * adds new item to the randomized queue after the already added items
+     * Add new item to the randomized queue after the already added items.
      *
      * @param item - the item that will be added to the randomized queue
      */
     public void enqueue(Item item) {
         if (item == null) throw new IllegalArgumentException();
         if (size == queue.length) resize(2 * queue.length);
-        size++;
-        if (size == 1) tail = head;
-        else tail++;
-        if (queue.length == 0) queue = (Item[]) new Object[1];
-        queue[tail] = item;
+
+        if (queue.length == 0) queue = (Item[]) new Object[INIT_CAPACITY];
+        queue[size++] = item;
     }
 
+    private void resize(int capasity) {
+        Item[] copy = (Item[]) new Object[capasity];
+        for (int i = 0; i < size; i++) {
+            copy[i] = queue[i];
+        }
+        queue = copy;
+    }
 
     /**
-     * removes random element of the randomized queue
+     * Remove random element from the randomized queue.
      *
      * @return the item that was removed
      * @throws NoSuchElementException if the current stack is empty
@@ -60,27 +67,70 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         int index = StdRandom.uniformInt(size);
         Item removedItem = queue[index];
 
-        Item[] result = (Item[]) new Object[queue.length - 1];
-        if (index == 0) result = generateArray(1, size);
-        else if (index == size - 1) result = generateArray(0, size - 1);
-        else {
-            Item[] str1 = generateArray(0, index);
-            Item[] str2 = generateArray(index + 1, size);
-            System.arraycopy(str1, 0, result, 0, str1.length);
-            System.arraycopy(str2, 0, result, str1.length, str2.length);
+        // Item[] result = (Item[]) new Object[queue.length - 1];
+        //
+        // if (index == 0) {
+        //     Item[] tmp = (Item[]) new Object[queue.length - 1];
+        //     System.arraycopy(queue, index + 1, tmp, 0, tmp.length);
+        //     result = tmp;
+        //
+        // }
+        // else if (index < queue.length - 1) {
+        //     Item[] tmp1 = (Item[]) new Object[index];
+        //     System.arraycopy(queue, 0, tmp1, 0, tmp1.length);
+        //     Item[] tmp2 = (Item[]) new Object[queue.length - (index + 1)];
+        //     System.arraycopy(queue, index + 1, tmp2, 0, tmp2.length);
+        //
+        //     System.arraycopy(tmp1, 0, result, 0, tmp1.length);
+        //     System.arraycopy(tmp2, 0, result, tmp1.length, tmp2.length);
+        // }
+        // else {
+        //     Item[] tmp = (Item[]) new Object[index];
+        //     System.arraycopy(queue, 0, tmp, 0, tmp.length);
+        //     result = tmp;
+        // }
 
-        }
-        queue = result;
+        queue = updateInputQueueAfterRandomRemoval(index, queue);
+
         size--;
-        tail--;
 
         if (size > 0 && size == queue.length / 4) resize(queue.length / 2);
         return removedItem;
+    }
+
+
+    private Item[] updateInputQueueAfterRandomRemoval(int index, Item[] inputQueue) {
+        Item[] result = (Item[]) new Object[inputQueue.length - 1];
+
+        if (index == 0) {
+            // Item[] tmp = (Item[]) new Object[inputQueue.length - 1];
+            System.arraycopy(inputQueue, index + 1, result, 0, result.length);
+            // result = tmp;
+
+        }
+        else if (index < inputQueue.length - 1) {
+            Item[] tmp1 = (Item[]) new Object[index];
+            System.arraycopy(inputQueue, 0, tmp1, 0, tmp1.length);
+            Item[] tmp2 = (Item[]) new Object[inputQueue.length - (index + 1)];
+            System.arraycopy(inputQueue, index + 1, tmp2, 0, tmp2.length);
+
+            System.arraycopy(tmp1, 0, result, 0, tmp1.length);
+            System.arraycopy(tmp2, 0, result, tmp1.length, tmp2.length);
+        }
+        else {
+            Item[] tmp = (Item[]) new Object[index];
+            System.arraycopy(inputQueue, 0, tmp, 0, tmp.length);
+            result = tmp;
+        }
+
+        return result;
 
     }
 
     /**
-     * @return random element of the randomized queue without removing it
+     * Return random element from the randomized queue without removing it.
+     *
+     * @return random element from the randomized queue
      */
     public Item sample() {
         if (isEmpty()) throw new NoSuchElementException();
@@ -88,7 +138,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     /**
-     * returns an iterator to this deque that iterates through the items in FIFO order
+     * Return an iterator to this deque that iterates through the items in FIFO order.
      *
      * @return an iterator to this deque that iterates through the items in FIFO order
      */
@@ -97,12 +147,12 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     private class RandomizedQueueIterator implements Iterator<Item> {
-        private Item[] iteratorQueue;
+        private Item[] randomizedQueue;
         private int numOfIterations = size;
 
 
-        public RandomizedQueueIterator(Item[] queue) {
-            iteratorQueue = queue;
+        public RandomizedQueueIterator(Item[] input) {
+            randomizedQueue = input;
         }
 
         public boolean hasNext() {
@@ -112,31 +162,10 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         public Item next() {
             if (!hasNext()) throw new NoSuchElementException();
             int index = StdRandom.uniformInt(numOfIterations);
-            Item itemToShow = iteratorQueue[index];
-
-            iteratorQueue = helperMethod(index, iteratorQueue);
-
+            Item itemToShow = randomizedQueue[index];
+            randomizedQueue = updateInputQueueAfterRandomRemoval(index, randomizedQueue);
             numOfIterations--;
             return itemToShow;
-        }
-
-        private Item[] helperMethod(int index, Item[] inputQueue) {
-            Item[] result;
-            if (index == 0 && size == 1) result = (Item[]) new Object[] { inputQueue[0] };
-            else result = generateArray(index, inputQueue);
-
-            return result;
-        }
-
-        private Item[] generateArray(int index, Item[] inputQueue) {
-            Item[] result = (Item[]) new Object[inputQueue.length - 1];
-            for (int i = 0; i < index; i++) {
-                result[i] = inputQueue[i];
-            }
-            for (int i = index + 1; i < result.length; i++) {
-                result[i - 1] = inputQueue[i];
-            }
-            return result;
         }
 
         public void remove() {
@@ -152,46 +181,93 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     public String toString() {
         StringBuilder s = new StringBuilder();
         for (Item item : this) {
-            s.append(item);
-            s.append("\n");
+            if (item != null) {
+                s.append(item);
+                s.append("\n");
+            }
+
         }
 
         return s.toString();
     }
 
-    private void resize(int capasity) {
-        Item[] copy = (Item[]) new Object[capasity];
-        for (int i = 0; i < size; i++) {
-            copy[i] = queue[head + i];
-        }
-        queue = copy;
-        head = 0;
-        tail = size - 1;
-    }
-
-    private Item[] generateArray(int startIndex, int endIndex) {
-        Item[] result = (Item[]) new Object[(endIndex - startIndex)];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = queue[startIndex + i];
-        }
-        return result;
-    }
-
     public static void main(String[] args) {
-        RandomizedQueue<String> q = new RandomizedQueue<String>();
-        while (!StdIn.isEmpty()) {
-            String item = StdIn.readString();
-            if (!item.equals("-")) {
-                q.enqueue(item);
-            }
-            else if (!q.isEmpty()) {
-                StdOut.println(q.dequeue());
-                System.out.println("(" + q.size() + "left on queue)");
-            }
+        // RandomizedQueue<String> q = new RandomizedQueue<String>();
+        // while (!StdIn.isEmpty()) {
+        //     String item = StdIn.readString();
+        //     if (!item.equals("-")) {
+        //         q.enqueue(item);
+        //     }
+        //     else if (!q.isEmpty()) {
+        //         StdOut.println(q.dequeue());
+        //         System.out.println("(" + q.size() + "left on queue)");
+        //     }
+        // }
+        // // System.out.println(q.toString());
+        // for (String str : q) {
+        //     System.out.println(str);
+        //     System.out.println("---");
+        // }
+        // System.out.println("(" + q.size() + "left on queue)");
+        //
+
+
+        // --------------
+
+        RandomizedQueue<Integer> testQueue = new RandomizedQueue<>();
+
+        // testQueue.enqueue(170);
+        // testQueue.enqueue(402);
+        // testQueue.enqueue(243);
+
+        // System.out.println(testQueue.iterator());
+        // for (Iterator<Integer> it = testQueue.iterator(); it.hasNext(); ) {
+        //     int i = it.next();
+        //     System.out.println(i);
+        //
+        // }
+
+        // testQueue.enqueue(336);
+        // testQueue.enqueue(315);
+        // testQueue.enqueue(161);
+        // testQueue.enqueue(75);
+        // testQueue.enqueue(435);
+        // testQueue.enqueue(66);
+
+        testQueue.enqueue(480);
+        testQueue.enqueue(39);
+        testQueue.enqueue(814);
+        testQueue.enqueue(585);
+        testQueue.enqueue(568);
+        testQueue.enqueue(312);
+        testQueue.enqueue(488);
+        testQueue.enqueue(666);
+        // System.out.println(testQueue.dequeue());
+        // System.out.println(testQueue.dequeue());
+        // System.out.println(testQueue.dequeue());
+        // System.out.println(testQueue.dequeue());
+        // System.out.println(testQueue.dequeue());
+        // System.out.println(testQueue.dequeue());
+        // System.out.println(testQueue.dequeue());
+        // System.out.println(testQueue.dequeue());
+        System.out.println();
+        // System.out.println("sample:" + testQueue.sample());
+        System.out.println(testQueue.size());
+        System.out.println();
+        System.out.println("to string");
+        System.out.println(testQueue.toString());
+
+
+        System.out.println("Iterator");
+        for (Iterator<Integer> it = testQueue.iterator(); it.hasNext(); ) {
+            // for (Integer pl : testQueue) {
+            //     System.out.println("inner foreach");
+            //     System.out.println(pl);
+            // }
+            int i = it.next();
+            System.out.println(i);
         }
-        System.out.println(q.toString());
-        System.out.println("(" + q.size() + "left on queue)");
+
+
     }
-
-
 }
